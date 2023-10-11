4f46f41d2a15fbdd799dff44ef69e41c1881188d
“Mbaye <seckm105@gmail.com>
2023-10-11 16:32:25 +0000

    30 days for JS - day 8 - les objets

---
+++ 30J - js/08_Day_Objects/08_day_starter/scripts/main.js
@@ -0,0 +1,395 @@
+//scope.js
+a = 'JavaScript'; // declaring a variable without let or const make it available in window object and this found anywhere
+b = 10; // this is a global scope variable and found in the window object
+function letsLearnScope() {
+  console.log(a, b);
+  if (true) {
+    console.log(a, b);
+  }
+}
+console.log(a, b); // accessible
+
+//scope.js
+let c = 'JavaScript'; // is a global scope it will be found anywhere in this file
+let d = 10; // is a global scope it will be found anywhere in this file
+function letsLearnScope() {
+  console.log(a, b); // JavaScript 10, accessible
+  if (true) {
+    letca = 'Python';
+    let d = 100;
+    console.log(a, b); // Python 100
+  }
+  console.log(a, b);
+}
+letsLearnScope();
+console.log(c, d); // JavaScript 10, accessible
+
+//scope.js
+let a = 'JavaScript'; // is a global scope it will be found anywhere in this file
+let b = 10; // is a global scope it will be found anywhere in this file
+
+// Function scope
+function letsLearnScope() {
+  console.log(a, b); // JavaScript 10, accessible
+  let value = false;
+  // block scope
+  if (true) {
+    // we can access from the function and outside the function but
+    // variables declared inside the if will not be accessed outside the if block
+    let a = 'Python';
+    let b = 20;
+    let c = 30;
+    let d = 40;
+    value = !value;
+    console.log(a, b, c, value); // Python 20 30 true
+  }
+  // we can not access c because c's scope is only the if block
+  console.log(a, b, value); // JavaScript 10 true
+}
+letsLearnScope();
+console.log(a, b); // JavaScript 10, accessible
+
+//scope.js
+function letsLearnScope() {
+  var gravity = 9.81;
+  console.log(gravity);
+}
+// console.log(gravity), Uncaught ReferenceError: gravity is not defined
+
+if (true) {
+  var gravity = 9.81;
+  console.log(gravity); // 9.81
+}
+console.log(gravity); // 9.81
+
+for (var i = 0; i < 3; i++) {
+  console.log(i); // 0, 1, 2
+}
+console.log(i); // 3
+
+//scope.js
+function letsLearnScope() {
+  // you can use let or const, but gravity is constant I prefer to use const
+  const gravity = 9.81;
+  console.log(gravity);
+}
+// console.log(gravity), Uncaught ReferenceError: gravity is not defined
+
+if (true) {
+  const gravity = 9.81;
+  console.log(gravity); // 9.81
+}
+// console.log(gravity), Uncaught ReferenceError: gravity is not defined
+
+for (let i = 0; i < 3; i++) {
+  console.log(i); // 0, 1, 2
+}
+// console.log(i), Uncaught ReferenceError: i is not defined
+
+const person = {};
+
+const rectangle = {
+  length: 20,
+  width: 20,
+};
+console.log(rectangle); // {length: 20, width: 20}
+
+const person2 = {
+  firstName: 'Asabeneh',
+  lastName: 'Yetayeh',
+  age: 250,
+  country: 'Finland',
+  city: 'Helsinki',
+  skills: [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Node',
+    'MongoDB',
+    'Python',
+    'D3.js',
+  ],
+  isMarried: true,
+};
+console.log(person2);
+
+const person3 = {
+  firstName: 'Asabeneh',
+  lastName: 'Yetayeh',
+  age: 250,
+  country: 'Finland',
+  city: 'Helsinki',
+  skills: [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Node',
+    'MongoDB',
+    'Python',
+    'D3.js',
+  ],
+  getFullName: function () {
+    return `${this.firstName}${this.lastName}`;
+  },
+  'phone number': '+3584545454545',
+};
+
+// accessing values using .
+console.log(person3.firstName);
+console.log(person3.lastName);
+console.log(person3.age);
+console.log(person3.location); // undefined
+
+// value can be accessed using square bracket and key name
+console.log(person3['firstName']);
+console.log(person3['lastName']);
+console.log(person3['age']);
+console.log(person3['age']);
+console.log(person3['location']); // undefined
+
+// for instance to access the phone number we only use the square bracket method
+console.log(person3['phone number']);
+
+const person4 = {
+  firstName: 'Asabeneh',
+  lastName: 'Yetayeh',
+  age: 250,
+  country: 'Finland',
+  city: 'Helsinki',
+  skills: [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Node',
+    'MongoDB',
+    'Python',
+    'D3.js',
+  ],
+  getFullName: function () {
+    return `${this.firstName} ${this.lastName}`;
+  },
+};
+
+console.log(person4.getFullName());
+
+const person5 = {
+  firstName: 'Asabeneh',
+  lastName: 'Yetayeh',
+  age: 250,
+  country: 'Finland',
+  city: 'Helsinki',
+  skills: [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Node',
+    'MongoDB',
+    'Python',
+    'D3.js',
+  ],
+  getFullName: function () {
+    return `${this.firstName} ${this.lastName}`;
+  },
+};
+person5.nationality = 'Ethiopian';
+person5.country = 'Finland';
+person5.title = 'teacher';
+person5.skills.push('Meteor');
+person5.skills.push('SasS');
+person5.isMarried = true;
+
+person5.getPersonInfo = function () {
+  let skillsWithoutLastSkill = this.skills
+    .splice(0, this.skills.length - 1)
+    .join(', ');
+  let lastSkill = this.skills.splice(this.skills.length - 1)[0];
+
+  let skills = `${skillsWithoutLastSkill}, and ${lastSkill}`;
+  let fullName = this.getFullName();
+  let statement = `${fullName} is a ${this.title}.\nHe lives in ${this.country}.\nHe teaches ${skills}.`;
+  return statement;
+};
+console.log(person5);
+console.log(person5.getPersonInfo());
+
+const person = {
+  firstName: 'Asabeneh',
+  age: 250,
+  country: 'Finland',
+  city: 'Helsinki',
+  skills: ['HTML', 'CSS', 'JS'],
+  title: 'teacher',
+  address: {
+    street: 'Heitamienkatu 16',
+    pobox: 2002,
+    city: 'Helsinki',
+  },
+  getPersonInfo: function () {
+    return `I am ${this.firstName} and I live in ${this.city}, ${this.country}. I am ${this.age}.`;
+  },
+};
+
+//Object methods: Object.assign, Object.keys, Object.values, Object.entries
+//hasOwnProperty
+
+const copyPerson = Object.assign({}, person);
+console.log(copyPerson);
+
+const keys = Object.keys(copyPerson);
+console.log(keys); //['firstName', 'age', 'country','city', 'skills','title', 'address', 'getPersonInfo']
+const address = Object.keys(copyPerson.address);
+console.log(address); //['street', 'pobox', 'city']
+
+const values = Object.values(copyPerson);
+console.log(values);
+
+const entries = Object.entries(copyPerson);
+console.log(entries);
+
+console.log(copyPerson.hasOwnProperty('name'));
+console.log(copyPerson.hasOwnProperty('score'));
+
+const users = {
+  Alex: {
+    email: 'alex@alex.com',
+    skills: ['HTML', 'CSS', 'JavaScript'],
+    age: 20,
+    isLoggedIn: false,
+    points: 30,
+  },
+  Asab: {
+    email: 'asab@asab.com',
+    skills: [
+      'HTML',
+      'CSS',
+      'JavaScript',
+      'Redux',
+      'MongoDB',
+      'Express',
+      'React',
+      'Node',
+    ],
+    age: 25,
+    isLoggedIn: false,
+    points: 50,
+  },
+  Brook: {
+    email: 'daniel@daniel.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+    age: 30,
+    isLoggedIn: true,
+    points: 50,
+  },
+  Daniel: {
+    email: 'daniel@alex.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+    age: 20,
+    isLoggedIn: false,
+    points: 40,
+  },
+  John: {
+    email: 'john@john.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+    age: 20,
+    isLoggedIn: true,
+    points: 50,
+  },
+  Thomas: {
+    email: 'thomas@thomas.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+    age: 20,
+    isLoggedIn: false,
+    points: 40,
+  },
+  Paul: {
+    email: 'paul@paul.com',
+    skills: [
+      'HTML',
+      'CSS',
+      'JavaScript',
+      'MongoDB',
+      'Express',
+      'React',
+      'Node',
+    ],
+    age: 20,
+    isLoggedIn: false,
+    points: 40,
+  },
+};
+const users = [
+  {
+    _id: 'ab12ex',
+    username: 'Alex',
+    email: 'alex@alex.com',
+    password: '123123',
+    createdAt: '08/01/2020 9:00 AM',
+    isLoggedIn: false,
+  },
+  {
+    _id: 'fg12cy',
+    username: 'Asab',
+    email: 'asab@asab.com',
+    password: '123456',
+    createdAt: '08/01/2020 9:30 AM',
+    isLoggedIn: true,
+  },
+  {
+    _id: 'zwf8md',
+    username: 'Brook',
+    email: 'brook@brook.com',
+    password: '123111',
+    createdAt: '08/01/2020 9:45 AM',
+    isLoggedIn: true,
+  },
+  {
+    _id: 'eefamr',
+    username: 'Martha',
+    email: 'martha@martha.com',
+    password: '123222',
+    createdAt: '08/01/2020 9:50 AM',
+    isLoggedIn: false,
+  },
+  {
+    _id: 'ghderc',
+    username: 'Thomas',
+    email: 'thomas@thomas.com',
+    password: '123333',
+    createdAt: '08/01/2020 10:00 AM',
+    isLoggedIn: false,
+  },
+];
+
+const products = [
+  {
+    _id: 'eedfcf',
+    name: 'mobile phone',
+    description: 'Huawei Honor',
+    price: 200,
+    ratings: [
+      { userId: 'fg12cy', rate: 5 },
+      { userId: 'zwf8md', rate: 4.5 },
+    ],
+    likes: [],
+  },
+  {
+    _id: 'aegfal',
+    name: 'Laptop',
+    description: 'MacPro: System Darwin',
+    price: 2500,
+    ratings: [],
+    likes: ['fg12cy'],
+  },
+  {
+    _id: 'hedfcg',
+    name: 'TV',
+    description: 'Smart TV:Procaster',
+    price: 400,
+    ratings: [{ userId: 'fg12cy', rate: 5 }],
+    likes: ['fg12cy'],
+  },
+];

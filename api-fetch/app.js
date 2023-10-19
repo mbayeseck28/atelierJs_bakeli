bcf572e0bce650ca15f2bc9d800c9341ccdd4afc
“Mbaye <seckm105@gmail.com>
2023-10-19 21:54:55 +0000

    Projet1 API fetch - first commit

--- api-fetch/app.js
+++ api-fetch/app.js
@@ -1,14 +1,25 @@
-// Fonction pour récupérer les données de l'API
-function fetchTodos() {
-  fetch('https://jsonplaceholder.typicode.com/todos/')
+let listTodoNonComplet = [];
+let listTodoComplet = [];
+// Fonction pour récupérer les données de l'API non complétés
+function fetchTodosNonCompleted() {
+  fetch('https://jsonplaceholder.typicode.com/todos')
     .then((response) => response.json())
     .then((data) => {
       const todoList = document.getElementById('todo-list');
       data.forEach((todo) => {
-        const listItem = document.createElement('li');
-        listItem.classList.add('list-group-item');
-        listItem.textContent = `ID: ${todo.id}, Titre: ${todo.title}, Complété: ${todo.completed}`;
-        todoList.appendChild(listItem);
+        if (todo.completed == false) {
+          const listItem = document.createElement('li');
+          listItem.classList.add('list-group-item');
+          listItem.id = todo.id;
+          listTodoNonComplet.push(todo);
+          listItem.innerHTML = `
+          Todo: <span class="fw-bold">${todo.title}</span> - Completed: <span class="fw-bold">${todo.completed}</span>
+          <div class="btn-group float-right">
+            <button type="button" class="btn btn-success" onclick="completeTodo(${todo.id})">True</button>
+          </div>
+          `;
+          todoList.appendChild(listItem);
+        }
       });
     })
     .catch((error) => {
@@ -18,17 +29,106 @@ function fetchTodos() {
       );
     });
 }
+// Fonction pour récupérer les données de l'API complétés
+function completeTodo(todoId) {
+  const todoList = document.getElementById('todoId');
+  console.log(todoList);
+  const cleRecherche = 'id';
+  const valeurRecherche = todoId;
+  const objetTrouve = listTodoNonComplet.find(
+    (objet) => objet[cleRecherche] === valeurRecherche
+  );
+  objetTrouve.completed = true;
+  if (objetTrouve) {
+    listTodoComplet.push(objetTrouve);
+    const todoCompleted = document.getElementById('todoCompleted');
+    let chiffre = document.querySelector('.chiffre');
+    if (objetTrouve.completed == true) {
+      const list = document.createElement('li');
+      list.classList.add('list-group-item');
+      list.id = objetTrouve.id;
+      list.innerHTML = `
+      Todo: <span class="fw-bold">${objetTrouve.title}</span> - Completed: <span class="fw-bold">${objetTrouve.completed}</span>
+      <div class="btn-group float-right">
+        <button type="button" onclick="details(${objetTrouve.id})" class="btn btn-primary">Détails</button>
+        <button type="button" onclick="falser(${objetTrouve.id})" class="btn btn-danger">False</button>
+      </div>
+      `;
+      todoCompleted.appendChild(list);
+    }
+    chiffre.textContent = listTodoComplet.length;
+    // console.log({ listTodoComplet });
+  } else {
+    console.log('Aucun objet trouvé avec la clé de valeur recherchée.');
+  }
+}
 
-// Appeler la fonction pour afficher les données au chargement de la page
-fetchTodos();
+function details(todoId) {
+  const cleRecherche = 'id';
+  const valeurRecherche = todoId;
+  const objetTrouve = listTodoNonComplet.find(
+    (objet) => objet[cleRecherche] === valeurRecherche
+  );
+  objetTrouve.completed = true;
+  if (objetTrouve) {
+    console.log(objetTrouve);
+  } else {
+    console.log('Aucun objet trouvé avec la clé de valeur recherchée.');
+  }
+}
 
-// async function fetchTodo() {
-//   const r = await fetch('https://jsonplaceholder.typicode.com/todos/');
-//   if (r.ok === true) {
-//     return r.json();
-//   }
-//   throw new Error('Impossible de contacter le serveur');
-// }
-// fetchTodo().then((todo) => console.log(todo));
+// ____________________________________________
+// Appeler les fonction pour afficher les données au chargement de la page
+fetchTodosNonCompleted();
+console.log(listTodoNonComplet);
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+// ____________________________________
 
 // Fonction pour récupérer les données de l'API
+
+// let todos = [];
+// fetch('https://jsonplaceholder.typicode.com/todos')
+//   .then((response) => response.json())
+//   .then((res) => {
+//     todos = res;
+//   });
+// console.log(todos);
+
+// _________
+
+// const fetchTodo = async () => {
+//   let response = await fetch('https://jsonplaceholder.typicode.com/todos');
+//   try {
+//     return response.json();
+//   } catch (error) {
+//     return 'Error';
+//   }
+//   // throw new Error('Impossible de recuperer les données');
+// };
+// // console.log(todos);
+// fetchTodo().then((todo) => console.log(todo));
+// // console.log('Seck');

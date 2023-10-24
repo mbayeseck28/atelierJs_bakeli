d8ca1885349c3a56386c9d31cb68004462698411
“Mbaye <seckm105@gmail.com>
2023-10-24 16:47:36 +0000

    My App version 2

--- api-fetch/app.js
+++ api-fetch/app.js
@@ -1,11 +1,15 @@
+// Variables globaux
 let listTodoNonComplet = [];
 let listTodoComplet = [];
+
 // Fonction pour récupérer les données de l'API non complétés
 function fetchTodosNonCompleted() {
   fetch('https://jsonplaceholder.typicode.com/todos')
     .then((response) => response.json())
     .then((data) => {
       const todoList = document.getElementById('todo-list');
+      // let btnBack = document.querySelector('.btnBack');
+      // btnBack.classList.add('d-none');
       data.forEach((todo) => {
         if (todo.completed == false) {
           listTodoNonComplet.push(todo);
@@ -26,9 +30,10 @@ function fetchTodosNonCompleted() {
       );
     });
 }
-// Fonction pour récupérer les données de l'API complétés
+// Fonction pour passer le completed de false à true
 function completeTodo(todoId) {
   // const todoList = document.getElementById('todoId');
+  let chiffre = document.querySelector('.chiffre');
   const cleRecherche = 'id';
   const valeurRecherche = todoId;
   const objetTrouve = listTodoNonComplet.find(
@@ -37,17 +42,6 @@ function completeTodo(todoId) {
   objetTrouve.completed = true;
   if (objetTrouve) {
     listTodoComplet.push(objetTrouve);
-    const todoCompleted = document.getElementById('todoCompleted');
-    let chiffre = document.querySelector('.chiffre');
-    if (objetTrouve.completed == true) {
-      const tr = document.createElement('tr');
-      tr.innerHTML = `
-            <td>${objetTrouve.title}</td> <td>${objetTrouve.completed}</td> 
-            <td><button type="button" class="btn btn-danger sup" onclick="falser(${objetTrouve.id})">retourner</button></td>              
-            <td><button type="button"data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions" class="btn btn-primary det" onclick="details(${objetTrouve.id})">Détails</button></td>              
-            `;
-      todoCompleted.appendChild(tr);
-    }
     chiffre.textContent = listTodoComplet.length;
     document.querySelector('#todo-list').addEventListener('click', (e) => {
       target = e.target;
@@ -61,6 +55,48 @@ function completeTodo(todoId) {
   }
 }
 
+// Afficher la page des tache completer
+let boutonNot = document.querySelector('.boutonNot');
+boutonNot.addEventListener('click', (e) => {
+  const list = document.getElementById('todo-list');
+  let alert = document.getElementById('alert');
+  alert.classList.add('d-none');
+  let btnBack = document.querySelector('.btnBack');
+  btnBack.classList.remove('d-none');
+  list.innerHTML = '';
+  // const container = document.querySelector('.container')
+
+  listTodoComplet.forEach((todo) => {
+    if (todo.completed == true) {
+      console.log(todo);
+      listTodoNonComplet.push(todo);
+      const list = document.querySelector('#todo-list');
+      const tr = document.createElement('tr');
+      tr.innerHTML = `
+            <td>${todo.title}</td> <td>${todo.completed}</td>
+            <td>
+              <button type="button" class="btn btn-danger sup" onclick="nonComplet(${todo.id})">Non completer</button>
+              <button type="button"data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions" class="btn btn-primary det" onclick="details(${todo.id})">Détails</button>
+            </td>
+             `;
+      list.appendChild(tr);
+    }
+  });
+});
+// Bouton retour
+const btnBack = document.querySelector('.btnBack');
+btnBack.addEventListener('click', (e) => {
+  // window.location.href = './index.html';
+  const list = document.getElementById('todo-list');
+  list.innerHTML = '';
+  let alert = document.getElementById('alert');
+  alert.classList.remove('d-none');
+  // let btnBack = document.getElementById('btnBack');
+  // btnBack.classList.add('d-none');
+  fetchTodosNonCompleted();
+});
+
+// Afficher les détails d'une tache
 function details(todoId) {
   const cleRecherche = 'id';
   const valeurRecherche = todoId;
@@ -69,9 +105,8 @@ function details(todoId) {
   );
   objetTrouve.completed = true;
   if (objetTrouve) {
-    let modal = document.querySelector('#exampleModal');
-    let offcanvas = document.querySelector('.offcanvas');
     const div = document.querySelector('.offcanvas-body');
+    div.innerHTML = '';
     const ul = document.createElement('ul');
     ul.classList.add('list-group');
     ul.innerHTML = `
@@ -91,55 +126,3 @@ function details(todoId) {
 // ____________________________________________
 // Appeler les fonction pour afficher les données au chargement de la page
 fetchTodosNonCompleted();
-console.log(listTodoNonComplet);
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-
-// ____________________________________
-
-// Fonction pour récupérer les données de l'API
-
-// let todos = [];
-// fetch('https://jsonplaceholder.typicode.com/todos')
-//   .then((response) => response.json())
-//   .then((res) => {
-//     todos = res;
-//   });
-// console.log(todos);
-
-// _________
-
-// const fetchTodo = async () => {
-//   let response = await fetch('https://jsonplaceholder.typicode.com/todos');
-//   try {
-//     return response.json();
-//   } catch (error) {
-//     return 'Error';
-//   }
-//   // throw new Error('Impossible de recuperer les données');
-// };
-// // console.log(todos);
-// fetchTodo().then((todo) => console.log(todo));
-// // console.log('Seck');
